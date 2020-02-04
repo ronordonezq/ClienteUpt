16d240d89f10c7fac2921f6987a562b0f2f2aba2
Ronald Ordoñez <37679541+ronordonezq@users.noreply.github.com>
2020-02-04 18:36:43 -0500

    actualizacion de visualizar conductor

--- app/src/main/java/com/example/clienteupt/MainActivity.java
+++ app/src/main/java/com/example/clienteupt/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.clienteupt;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,7 +15,10 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,6 +26,7 @@ import org.json.JSONObject;
 
 import io.socket.client.Ack;
 import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class MainActivity  extends AppCompatActivity implements OnMapReadyCallback {
   LatLng pos;
@@ -35,9 +41,9 @@ public class MainActivity  extends AppCompatActivity implements OnMapReadyCallba
             getSupportFragmentManager().findFragmentById(R.id.mapa);
     mapFragment.getMapAsync(this);
     mSocket = App.getSocket();
-//mSocket.on("taxiencontrado", taxiencontrado);
-//mSocket.on("localizacion",localizacion);
-//mSocket.on("Abordo",abordo);
+    mSocket.on("taxiencontrado", taxiencontrado);
+    mSocket.on("localizacion",localizacion);
+    mSocket.on("Abordo",abordo);
     mSocket.connect();
   }
 
@@ -63,6 +69,7 @@ public class MainActivity  extends AppCompatActivity implements OnMapReadyCallba
       } catch (JSONException e) {
         Log.e("JSONExceptionPresenter", e.toString());
       }
+      //Ack escucha una respuesta del servidor
       mSocket.emit("pedirtaxi", miubicacion, new Ack() {
         @Override
         public void call(Object... args) {
@@ -75,4 +82,92 @@ public class MainActivity  extends AppCompatActivity implements OnMapReadyCallba
     else
       Toast.makeText(this,"no se ha encontrado su ubicación",Toast.LENGTH_SHORT).show();
   }
+
+  String miconductor="";
+  private Emitter.Listener taxiencontrado = new Emitter.Listener() {
+    @Override
+    public void call(Object... args) {
+      Log.i("misdatos","taxiencontrado");
+      final JSONObject paramsRequest = (JSONObject) args[0];
+      runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+          try {
+            Log.i("misdatos","check:"+paramsRequest);
+            miconductor = paramsRequest.getString("datotaxi");
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage("Felicidades el señor " + miconductor + " atendera su pedido")
+                    .setTitle("Conductor encontrado")
+                    .setCancelable(false)
+                    .setNeutralButton("Aceptar",
+                            new DialogInterface.OnClickListener() {
+                              public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                              }
+                            });
+            AlertDialog alert = builder.create();
+            alert.show();
+          } catch(
+                  JSONException e)
+          {
+            Log.e("JSONException", e.toString());
+          }
+        }
+      });
+    }
+  };
+
+  Marker mimarker;
+  private Emitter.Listener localizacion = new Emitter.Listener() {
+    @Override
+    public void call(Object... args) {
+      Log.i("localizacion","nuevalocalizacion");
+      JSONObject paramsRequest = (JSONObject) args[0];
+      final Double latcond,loncond;
+      try {
+        Log.i("localizacion","nuevalocalizacion:"+paramsRequest.toString());
+        latcond = paramsRequest.getDouble("lat");
+        loncond = paramsRequest.getDouble("lon");
+        runOnUiThread(new Runnable() {
+          @Override
+          public void run() {
+            if(mimarker!=null)
+            {
+              mimarker.remove();
+            }
+            mimarker= mapa.addMarker(new MarkerOptions().position(new LatLng(latcond, loncond))
+                    .icon(BitmapDescriptorFactory
+                            .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)).title(miconductor));
+          }
+        });
+      } catch (JSONException e) {
+        Log.e("JSONException", e.toString());
+      }
+    }
+  };
+
+  private Emitter.Listener abordo = new Emitter.Listener() {
+    @Override
+    public void call(Object... args) {
+      runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+          AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+          builder.setMessage("Que tenga un buen viaje")
+                  .setTitle("Gracias por su preferencia")
+                  .setCancelable(false)
+                  .setNeutralButton("Aceptar",
+                          new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                              dialog.cancel();
+                            }
+                          });
+
+          AlertDialog alert = builder.create();
+          alert.show();
+        }
+      });
+    }
+  };
+
 }
